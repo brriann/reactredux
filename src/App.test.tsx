343e16ba54c270c9b1a905f6616d4cf71fa49c4d
Brian Foster <brriann@gmail.com>
2021-03-22 08:16:31 -0600

    remove cra boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test('expect 1 to be 1', () => {
+  // const { getByText } = render(
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+  // );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(1).toBe(1);
+  // expect(getByText(/learn/i)).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,16 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      {/* <CssBaseline /> */}
+      <CssBaseline />
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>App goes here!</h1>
         <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      </div>
     </React.Fragment>
   );
 }
