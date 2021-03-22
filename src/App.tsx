2d1605d7f2613d7b5089573e4a2e9b834bae5c80
Brian Foster <brriann@gmail.com>
2021-03-22 08:52:06 -0600

    css and mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import './App.css';
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from './theme';
 
 const HomePageComponent = lazy(() => import("./pages/HomePage"));
 const Page1Component = lazy(() => import("./pages/Page1"));
@@ -11,23 +11,25 @@ const Page3Component = lazy(() => import("./pages/Page3"));
 function App() {
   return (
     <BrowserRouter>
-      <CssBaseline />
-      <Suspense fallback={<Fragment />}>
-        <Switch>
-          <Route path="/page1">
-            <Page1Component />
-          </Route>
-          <Route path="/page2">
-            <Page2Component />
-          </Route>
-          <Route path="/page3">
-            <Page3Component />
-          </Route>
-          <Route exact path="/">
-            <HomePageComponent />
-          </Route>
-        </Switch>
-      </Suspense>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Suspense fallback={<Fragment />}>
+          <Switch>
+            <Route path="/page1">
+              <Page1Component />
+            </Route>
+            <Route path="/page2">
+              <Page2Component />
+            </Route>
+            <Route path="/page3">
+              <Page3Component />
+            </Route>
+            <Route exact path="/">
+              <HomePageComponent />
+            </Route>
+          </Switch>
+        </Suspense>
+      </MuiThemeProvider>
     </BrowserRouter>
   );
 }
