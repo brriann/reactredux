df3db76d0c704a9ddc23a9c6cf9c1e85c919146f
Brian Foster <brriann@gmail.com>
2021-03-22 07:41:21 -0600

    mobile first and cssbaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+      {/* <CssBaseline /> */}
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
@@ -52,6 +55,7 @@ function App() {
         </span>
       </header>
     </div>
+    </React.Fragment>
   );
 }
 
